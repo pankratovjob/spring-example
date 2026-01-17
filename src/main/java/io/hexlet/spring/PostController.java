7754a8a422a41b8741483541e08f930a92cc45ea
vetPan <pankratovjob@ya.ru>
2026-01-17 09:26:00 +0300

    add pagePagination

--- src/main/java/io/hexlet/spring/PostController.java
+++ src/main/java/io/hexlet/spring/PostController.java
@@ -4,6 +4,10 @@ import io.hexlet.spring.Model.Post;
 import io.hexlet.spring.repository.PostRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -25,10 +29,18 @@ public class PostController {
         return ResponseEntity.status(HttpStatus.CREATED).body(saved);
 
     }
-
+/*
     @GetMapping("posts")
     public List<Post> returnAll() {
         return postRepository.findAll();
+    }
+
+ */
+    @GetMapping("/posts")
+    public Page<Post> getPublishedPosts(@RequestParam(defaultValue = "0") int page,
+                                        @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+        return postRepository.findByPublishedTrue(pageable);
     }
 
     @GetMapping("posts/{id}")
@@ -62,3 +74,7 @@ public class PostController {
     }
 
 }
+
+
+
+
